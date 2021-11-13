2f679432411a5071b47d3789fc1cc131c0ad888f
PieKok <pierre.kokou@hotmail.com>
2021-11-13 18:42:12 +0100

    Logic for button to ask a street and then reveal its position; put a marker where you double-tap

--- memorymapview.py
+++ memorymapview.py
@@ -7,23 +7,9 @@ class MemoryMapView(MapView):
         if self.collide_point(*touch.pos) and touch.is_double_tap:
             print('Touch is a double tap !')
             print(touch)
-            print(' - distance between previous is', touch.double_tap_distance)
-            print(self.get_bbox())
-            print(self.width)
-            print(self.height)
-            x_random = self.width * random.random()
-            y_random = self.height * random.random()
-            print(' x = ', x_random)
-            print(' y = ', y_random)
-            print(self.get_latlon_at(x_random,y_random))
-            lonLat_coordinates = self.get_latlon_at(x_random, y_random)
-            marker1 = MapMarker(lat=lonLat_coordinates.lat, lon=lonLat_coordinates.lon)
-            self.add_marker(marker1)
-            print(marker1)
-            app = Nominatim(user_agent='StreetMemory')
-            address = app.reverse(lonLat_coordinates)
-            print(address.raw['address']['road'])
-
+            lonlat_coordinates = self.get_latlon_at(touch.x, touch.y)
+            my_marker = MapMarker(lat=lonlat_coordinates.lat, lon=lonlat_coordinates.lon)
+            self.add_marker(my_marker)
 
         else:
             super().on_touch_down(touch)
--- playScreen.py
+++ playScreen.py
@@ -1,9 +1,31 @@
 from kivy.uix.screenmanager import Screen
 import random
+from kivy_garden.mapview import MapMarker
+from geopy.geocoders import Nominatim
 from memorymapview import MemoryMapView
 
 
-
 class PlayScreen(Screen):
+    def __init__(self, **kw):
+        super().__init__(**kw)
+        self.x_target = 0
+        self.y_target = 0
+        self.lonlat_target = None
+        self.street_target = ""
+
     def actionButton(self):
-        pass
+        if self.ids.toolbar.ids.right_actions.children[0].icon == "play":
+            self.x_target = self.ids.mapViewer.width * random.random()
+            self.y_target = self.ids.mapViewer.height * random.random()
+            self.lonlat_target = self.ids.mapViewer.get_latlon_at(self.x_target, self.y_target)
+            my_geo_app = Nominatim(user_agent='StreetMemory')
+            my_address = my_geo_app.reverse(self.lonlat_target)
+            self.street_target = my_address.raw['address']['road']
+            self.ids.id_street_text.text = "  Double-tap where you think " + self.street_target + " is"
+            self.ids.toolbar.ids.right_actions.children[0].icon = "check-bold"
+
+        elif self.ids.toolbar.ids.right_actions.children[0].icon == "check-bold":
+            my_marker = MapMarker(lat=self.lonlat_target.lat, lon=self.lonlat_target.lon)
+            self.ids.mapViewer.add_marker(my_marker)
+            self.ids.toolbar.ids.right_actions.children[0].icon = "play"
+
