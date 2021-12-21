ff9830ddb34be722a432cfb73643164023a3e424
PieKok <pierre.kokou@hotmail.com>
2021-12-21 18:22:49 +0100

    Display target and guess street in a dialog pop-up; implement protection in case the target/guess address has no street

--- playScreen.py
+++ playScreen.py
@@ -3,6 +3,9 @@ import random
 from kivy_garden.mapview import MapMarker
 from geopy.geocoders import Nominatim
 from memorymapview import MemoryMapView
+from kivy.uix.boxlayout import BoxLayout
+from kivymd.uix.dialog import MDDialog
+from kivymd.uix.button import MDFlatButton
 
 
 class PlayScreen(Screen):
@@ -12,6 +15,7 @@ class PlayScreen(Screen):
         self.y_target = 0
         self.lonlat_target = None
         self.street_target = ""
+        self.results_dialog = None
 
     def actionButton(self):
         my_mapview = self.ids.mapViewer
@@ -21,6 +25,12 @@ class PlayScreen(Screen):
             self.lonlat_target = my_mapview.get_latlon_at(self.x_target, self.y_target)
             my_geo_app = Nominatim(user_agent='StreetMemory')
             my_address = my_geo_app.reverse(self.lonlat_target)
+            while not ("road" in my_address.raw['address']):
+                self.x_target = my_mapview.width * random.random()
+                self.y_target = my_mapview.height * random.random()
+                self.lonlat_target = my_mapview.get_latlon_at(self.x_target, self.y_target)
+                my_geo_app = Nominatim(user_agent='StreetMemory')
+                my_address = my_geo_app.reverse(self.lonlat_target)
             self.street_target = my_address.raw['address']['road']
             self.ids.id_street_text.text = "  Double-tap where you think " + self.street_target + " is"
             my_mapview.remove_target_marker()
@@ -30,4 +40,33 @@ class PlayScreen(Screen):
             my_mapview.set_target_marker(self.lonlat_target)
             self.ids.id_street_text.text = "  This is where " + self.street_target + " is"
             self.ids.toolbar.ids.right_actions.children[0].icon = "play"
+            self.results_dialog = MDDialog(
+                type="custom",
+                content_cls=ResultDialogContent(),
+                size_hint=(0.8, 0.1),
+                buttons=[
+                    MDFlatButton(
+                        text="Ok", on_release=self.close_results_dialog
+                    )
+                ]
+            )
+            self.results_dialog.open()
+            guess_street = self.get_guess_address()
+            my_str = 'Target: ' + self.street_target + '\nGuess: ' + guess_street
+            self.results_dialog.content_cls.ids.id_result_text.text = my_str
 
+    def get_guess_address(self):
+        my_mapview = self.ids.mapViewer
+        my_geo_app = Nominatim(user_agent='StreetMemory')
+        my_guess_address = my_geo_app.reverse([my_mapview.guess_marker.lat, my_mapview.guess_marker.lon])
+        if "road" in my_guess_address.raw['address']:
+            return my_guess_address.raw['address']['road']
+        else:
+            return "No street found"
+
+    def close_results_dialog(self, inst):
+        self.results_dialog.dismiss()
+
+
+class ResultDialogContent(BoxLayout):
+    pass
